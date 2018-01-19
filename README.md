08bdc2cd98e399d14b3955e21728186a11a3aeb2
Nils K <34674720+nkleinert@users.noreply.github.com>
2018-01-19 22:11:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MySecondLife-Updater
+Because some anti-virus programs report the updater as a virus, I decided to make the source code of this small but nice (:wink:) updater public.  
+What does the updater do? Not much. Downloads the latest version of the mod launcher (closed source code).
---
+++ src/Updater/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+
+namespace Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            Console.Title = "My2Life Updater";
+            Console.WriteLine("Lade Update herunter ...");
+            Console.WriteLine("");
+
+
+            Process[] ProcessName = Process.GetProcessesByName("MySecondLife - Modpack-Loader");
+
+            while(ProcessName.Length != 0)
+            {
+                Console.Write("\rWarte auf Beendigung vom Mod-Launcher ...");
+                ProcessName = Process.GetProcessesByName("MySecondLife - Modpack-Loader");
+                Thread.Sleep(100);
+            }
+
+            if (File.Exists(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\My2Life - Modpack-Loader.exe"))
+            {
+                File.Delete(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\My2Life - Modpack-Loader.exe");
+            }
+
+
+            using (WebClient Client = new WebClient())
+            {
+                Client.DownloadFileAsync(new Uri("https://mods.my2.life/update.exe"), Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\My2Life - Modpack-Loader.exe");
+                Client.DownloadProgressChanged += OnDownloadProgressChanged;
+                Client.DownloadFileCompleted += OnDownloadFileCompleted;
+            }
+
+            Console.Read();
+
+        }
+
+        private static void OnDownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (File.Exists(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\update.bat"))
+            {
+                File.Delete(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\update.bat");
+            }
+            Process.Start(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\My2Life - Modpack-Loader.exe");
+            Environment.Exit(0);
+        }
+
+        private static void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Console.Write("\rFortschritt: {0}%", e.ProgressPercentage);
+        }
+    }
+}
